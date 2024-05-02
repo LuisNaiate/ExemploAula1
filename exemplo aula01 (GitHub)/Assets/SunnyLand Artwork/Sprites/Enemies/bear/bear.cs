88fa3bd9015e77d9b92765244776d7cffb960b1b
LuisJahn <gustavo061108@gmail.com>
2024-05-02 11:38:51 -0300

    adicionei urso

--- exemplo aula01 (GitHub)/Assets/SunnyLand Artwork/Sprites/Enemies/bear/bear.cs
+++ exemplo aula01 (GitHub)/Assets/SunnyLand Artwork/Sprites/Enemies/bear/bear.cs
@@ -2,21 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class controlerplayes : MonoBehaviour
+public class bear : MonoBehaviour
 {
-    public float speed;
     public Rigidbody2D body;
-     public float horizontal;
+    public float speed;
+    public float horizontal;
+
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         horizontal = Input.GetAxis("Horizontal");
-        body.velocity = new Vector2 (horizontal * speed, body.velocity.y);
-        
+        body.velocity = new Vector2(horizontal * speed, body.velocity.y);
     }
 }
+
