d571f87b0e45b44f7cf9633d89d69873e63debaa
LuisJahn <gustavo061108@gmail.com>
2024-05-02 11:27:37 -0300

    movimentação
    coloquei movimentação e animação idle no player(ainda não tem transição)

---
+++ exemplo aula01 (GitHub)/Assets/SunnyLand Artwork/Sprites/player/idle/controlerplayes.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controlerplayes : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody2D body;
+     public float horizontal;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2 (horizontal * speed, body.velocity.y);
+    }
+}
